4852df3195e5821d58959956d2ec031b4e7187e1
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2023-12-19 20:35:19 +0800

    在不提供图片路径参数时进入配置窗口

---
+++ copy-image/Form2.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace copy_image
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.copy_image_1;
+            pictureBox1.Location = new System.Drawing.Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(64, 64);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(479, 289);
+            Controls.Add(pictureBox1);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "Form2";
+            Text = "配置";
+            Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
--- copy-image/Program.cs
+++ copy-image/Program.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+
 namespace copy_image
 {
     internal static class Program
@@ -5,8 +9,27 @@ namespace copy_image
         [STAThread] // 設定單執行緒單元(STA)模式，因為剪貼簿類需要它
         static void Main(string[] args)
         {
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1(args));
+            //ApplicationConfiguration.Initialize();
+            if (args.Length > 0 && IsValidFilePath(args[0]))
+            {
+                Form1 form1 = new Form1();
+                form1.imagePath = args[0];
+                Application.Run(form1);
+            }
+            else
+            {
+                Form2 form2 = new Form2();
+                if (IsValidFilePath(args[0]))
+                {
+                    form2.imagePath = args[0];
+                }
+                Application.Run(new Form2());
+            }
+        }
+
+        public static bool IsValidFilePath(string path)
+        {
+            return File.Exists(path) || Directory.Exists(path);
         }
     }
 }
